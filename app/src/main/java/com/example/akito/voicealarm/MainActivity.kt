930db9b2b5d9b3c2316bfb8b13019ef7d39208a2
Akito Mizuno <mznakt@gmail.com>
2019-01-19 18:42:28 +0900

    add dotinstall code

--- app/src/main/java/com/example/akito/voicealarm/MainActivity.kt
+++ app/src/main/java/com/example/akito/voicealarm/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.akito.voicealarm
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun changeTextView(view: View) {
+        messageTextView.text = "Hello there! Again!"
     }
 }
